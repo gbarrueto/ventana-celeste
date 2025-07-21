eac31c2f790b6f35458c3c4ecaf318221d188013
Eduardo <esep@uc.cl>
2025-07-21 12:25:35 -0400

    feat: slider blur

--- telescope/Slider/slider.js
+++ telescope/Slider/slider.js
@@ -1,22 +1,6 @@
 
-// const slider = document.getElementById('zoomSlider');
-// const value = document.getElementById('sliderValue');
+blurSlider.addEventListener('input', () => {
+    currentBlur = parseInt(blurSlider.value);
 
-// sliderValue.textContent = Math.exp(slider.value).toFixed(6);
-
-slider.addEventListener('input', () => {
-  // value.innerHTML = slider.value;
-  // e.preventDefault();
-  // if (e.touches.length === 1 && lastY !== null) {
-  //   const currentY = e.touches[0].clientY;
-  //   const deltaY = currentY - lastY;
-    // logFov += deltaY * sensitivity * 0.01;
-    // logFov = slider.value;
-    // logFov = Math.max(minLogFov, Math.min(maxLogFov, logFov));
-    logFov = parseFloat(slider.value);
-    updateDisplayFov();
-    // lastY = currentY;
-  // }
-  // logFov = slider.value;
-  // updateDisplayFov();
+    updateDisplayBlur();
 }); 
--- telescope/utils/updateDisplay.js
+++ telescope/utils/updateDisplay.js
@@ -2,12 +2,16 @@
 function updateDisplayFov() {
   const fov = Math.exp(logFov);
   fovDisplay.textContent = `FOV: ${fov.toFixed(6)}`;
-  // sliderValue.textContent = fov.toFixed(6);
-  slider.value = logFov;
   
-
   if (oldFov !== fov) {
     Protobject.Core.send({ f: fov }).to("index.html");
   }
   oldFov = fov;
+}
+
+
+function updateDisplayBlur() {
+  blurSlider.value = currentBlur;
+  
+  Protobject.Core.send({ blur: currentBlur }).to("index.html");
 }
