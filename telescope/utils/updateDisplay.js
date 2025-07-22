e435987f83b4acaee9b6ecb7f0d4661e7cbfcb09
Eduardo <esep@uc.cl>
2025-07-22 13:11:10 -0400

    feat: desenfocar al variar fov

--- telescope/utils/updateDisplay.js
+++ telescope/utils/updateDisplay.js
@@ -7,12 +7,28 @@ function updateDisplayFov() {
     Protobject.Core.send({ f: fov }).to("index.html");
   }
   oldFov = fov;
+
+  // Aplicar desenfoque
+  const zoomLevel = (Math.log(MAX_FOV) - logFov) / (Math.log(MAX_FOV) - Math.log(MIN_FOV));
+  const blurVariation = 1 + zoomLevel * 4;
+
+  blurTarget = 5 + (Math.random() - 0.5) * blurVariation;
+  blurTarget = Math.max(0, Math.min(10, blurTarget));
+
+  updateDisplayBlur();
 }
 
 
 function updateDisplayBlur() {
+  const diff = Math.abs(currentBlur - blurTarget);
+
+  const zoomLevel = (Math.log(MAX_FOV) - logFov) / (Math.log(MAX_FOV) - Math.log(MIN_FOV));
+  const sensitivity = 0.4 + zoomLevel * 1.6;
+
+  const blurEffect = Math.min(diff * sensitivity, 1) * 10;
+
   blurSlider.value = currentBlur;
   blurText.textContent = currentBlur;
   
-  Protobject.Core.send({ blur: currentBlur }).to("index.html");
+  Protobject.Core.send({ blur: blurEffect }).to("index.html");
 }
