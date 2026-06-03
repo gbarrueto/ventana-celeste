8952421e74dad39626cf37270618002e50ab6cb5
abellinouc <118542091+abellinouc@users.noreply.github.com>
2026-06-03 16:23:54 +0000

    prod

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,7 +1,9 @@
 export default {
   env: 'production',
-  smallDataPath: '/data/smalldata',
-  bigDataPath: '/data/bigdata',
+  smallDataPath: "https://smalldata.ventanaceleste.com/",
+  bigDataPath: "https://bigdata.ventanaceleste.com/",
+  //smallDataPath: '/data/smalldata/',
+  //bigDataPath: '/data/bigdata/',
   calibrateOnStart: true,
   enableZoomBlurTimer: true,
   enableDebugPanel: false,
